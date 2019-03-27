6ef5b16c5309787c34ef9a8dfc5fc9da096f6170
Lauren Woodrick <lauren@teamgaslight.com>
2019-03-27 18:14:54 -0400

    Plug in about graphic, adjust grid behavior.

--- assets/js/style-select.js
+++ assets/js/style-select.js
@@ -17,12 +17,12 @@ pickVisual.addEventListener("click", (event) => {
   stripSelected();
   event.srcElement.classList.add("selected");
   if (svgContainer.style.left == "-300%") {
-    svgContainer.setAttribute("style", "left: -415%;");
+    svgContainer.setAttribute("style", "left: -400%;");
     setTimeout(() => {
-      svgContainer.setAttribute("style", "transition: none; left: -115%;");
+      svgContainer.setAttribute("style", "transition: none; left: -100%;");
     }, 250);
   } else {
-    svgContainer.setAttribute("style", "left: -115%;");
+    svgContainer.setAttribute("style", "left: -100%;");
   }
 })
 
@@ -35,7 +35,7 @@ pickDev.addEventListener("click", (event) => {
 pickUx.addEventListener("click", (event) => {
   stripSelected();
   event.srcElement.classList.add("selected");
-  if (svgContainer.style.left == "-115%") {
+  if (svgContainer.style.left == "-100%") {
     svgContainer.setAttribute("style", "left: 0;");
     setTimeout(() => {
       svgContainer.setAttribute("style", "transition: none; left: -300%;");
@@ -44,3 +44,15 @@ pickUx.addEventListener("click", (event) => {
     svgContainer.setAttribute("style", "left: -300%;");
   }
 })
+
+const statsToggle = document.querySelector("#statsToggle");
+const statsContainer = document.querySelector("#statsContainer");
+
+statsToggle.addEventListener("click", (event) => {
+  statsToggle.classList.toggle("open");
+  statsContainer.classList.toggle("open");
+});
+statsContainer.addEventListener("click", (event) => {
+  statsToggle.classList.toggle("open");
+  statsContainer.classList.toggle("open");
+});
